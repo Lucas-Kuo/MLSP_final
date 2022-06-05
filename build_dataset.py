e316a16fab97c7b9d49629e875b3a6a3a14e506b
Lucas Kuo <74166753+Lucas-Kuo@users.noreply.github.com>
2022-06-06 02:04:52 +0800

    Create build_dataset.py

---
+++ build_dataset.py
@@ -0,0 +1,18 @@
+import config
+import shutil
+import os
+import pandas as pd
+
+if not os.path.exists(config.label_path):
+    raise FileNotFoundError(f"{config.label_path} does not exist. Please make sure to unzip the trainging dataset first.")
+df = pd.read_csv(config.label_path)
+
+for row in df.itertuples(index=False):
+    # row[0] is filename and row[1] is category
+    target_dir = os.path.sep.join([config.training_path, row[1]])
+    if not os.path.exists(target_dir):
+        print("[INFO] creating '{}' directory".format(target_dir))
+        os.makedirs(target_dir)
+    orig_file_path = os.path.sep.join([config.training_orig_path, row[0]])
+    new_file_path = os.path.sep.join([target_dir, row[0]])
+    shutil.copy2(orig_file_path, new_file_path)
