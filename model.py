92373ca9def772f9659072dc2ccb906524fddffc
Lucas Kuo <74166753+Lucas-Kuo@users.noreply.github.com>
2022-06-06 12:28:32 +0800

    Update model.py

--- model.py
+++ model.py
@@ -8,51 +8,7 @@ import os
 import numpy as np
 import config
 
-class ArcFace(Layer):
-    def __init__(self, n_classes=10, s=30.0, m=0.50, regularizer=None, **kwargs):
-        super(ArcFace, self).__init__(**kwargs)
-        self.n_classes = n_classes
-        self.s = s
-        self.m = m
-        self.regularizer = regularizers.get(regularizer)
-
-    def build(self, input_shape):
-        super(ArcFace, self).build(input_shape[0])
-        self.W = self.add_weight(name='W',
-                                shape=(input_shape[0][-1], self.n_classes),
-                                initializer='glorot_uniform',
-                                trainable=True,
-                                regularizer=self.regularizer)
-
-    def call(self, inputs):
-        x, y = inputs
-        c = K.shape(x)[-1]
-        # normalize feature
-        x = tf.nn.l2_normalize(x, axis=1)
-        # normalize weights
-        W = tf.nn.l2_normalize(self.W, axis=0)
-        # dot product
-        logits = x @ W
-        # add margin
-        # clip logits to prevent zero division when backward
-        theta = tf.acos(K.clip(logits, -1.0 + K.epsilon(), 1.0 - K.epsilon()))
-        target_logits = tf.cos(theta + self.m)
-        # sin = tf.sqrt(1 - logits**2)
-        # cos_m = tf.cos(logits)
-        # sin_m = tf.sin(logits)
-        # target_logits = logits * cos_m - sin * sin_m
-        #
-        logits = logits * (1 - y) + target_logits * y
-        # feature re-scale
-        logits *= self.s
-        out = tf.nn.softmax(logits)
-
-        return out
-
-    def compute_output_shape(self, input_shape):
-        return (None, self.n_classes)
-
-def build_effarc_model():
+def build_eff_model():
     base_model = tf.keras.applications.efficientnet_v2.EfficientNetV2M(include_top=False, weights='imagenet',
                                                                       input_shape=config.input_shape,)
 
@@ -74,9 +30,8 @@ def build_effarc_model():
     x = base_model(x, training=False)  # set training to False to avoid BN training
     x = tf.keras.layers.AveragePooling2D(pool_size=(15, 15))(x)
     x = tf.keras.layers.Flatten()(x)
-    x = tf.keras.layers.Dense(config.num_classes, activation=None,
+    output = tf.keras.layers.Dense(config.num_classes, activation="softmax",
                                     activity_regularizer=tf.keras.regularizers.L2(0.1))(x)
-    output = ArcFace(num_classes=config.num_classes)([x, label])
-    model = tf.keras.Model([inputs, label], output)
+    model = tf.keras.Model(inputs, output)
     
     return model
